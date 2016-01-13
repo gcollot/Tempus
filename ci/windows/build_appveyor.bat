cc32f7cb4836131d64bd6400ba13b0d36f59a4e7
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-13 11:11:14 +0100

    update av2

--- ci/windows/build_appveyor.bat
+++ ci/windows/build_appveyor.bat
@@ -1,7 +1,12 @@
 @echo off
 
+ECHO activating VS command prompt
+IF /I "%platform%"=="x64" ECHO x64 && CALL "C:\Program Files (x86)\Microsoft Visual Studio %msvs_toolset%.0\VC\vcvarsall.bat" amd64
+IF /I "%platform%"=="x86" ECHO x86 && CALL "C:\Program Files (x86)\Microsoft Visual Studio %msvs_toolset%.0\VC\vcvarsall.bat" x86
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
 echo Running cmake...
 cd c:\projects\tempus
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DOC=OFF -DBUILD_OSM2SHP=OFF -DBUILD_QGIS_PLUGIN -DBUILD_WPS=OFF
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DOC=OFF -DBUILD_OSM2SHP=OFF -DBUILD_QGIS_PLUGIN=OFF -DBUILD_WPS=OFF
 
 nmake
