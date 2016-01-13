bcd1bd7f840953df573679a7e8baf2ff4b99a946
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-13 11:58:11 +0100

    update av5

--- ci/windows/build_appveyor.bat
+++ ci/windows/build_appveyor.bat
@@ -5,6 +5,11 @@ IF /I "%platform%"=="x64" ECHO x64 && CALL "C:\Program Files (x86)\Microsoft Vis
 IF /I "%platform%"=="x86" ECHO x86 && CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+ECHO Downloading boost ...
+curl -o boost.exe -fsS "http://iweb.dl.sourceforge.net/project/boost/boost-binaries/1.59.0/boost_1_59_0-msvc-14.0-%platform%".exe
+boost.exe /DIR=C:\libs\boost_1_59_0 /verysilent
+set BOOST_ROOT="C:/libs/boost_1_9_0"
+
 echo Running cmake...
 cd c:\projects\tempus
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DOC=OFF -DBUILD_OSM2SHP=OFF -DBUILD_QGIS_PLUGIN=OFF -DBUILD_WPS=OFF
