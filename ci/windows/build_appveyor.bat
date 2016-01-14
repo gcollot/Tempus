d3eb8f85e6a3fba109f4a5e05d2163dcc5999d33
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-14 14:28:24 +0100

    appv2

--- ci/windows/build_appveyor.bat
+++ ci/windows/build_appveyor.bat
@@ -1,14 +1,14 @@
 @echo off
 
 ECHO activating VS command prompt
-IF /I "%platform%"=="x64" ECHO x64 && CALL "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" amd64
-IF /I "%platform%"=="x86" ECHO x86 && CALL "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" x86
+IF /I "%platform%"=="x64" ECHO x64 && CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+IF /I "%platform%"=="x86" ECHO x86 && CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 set BOOST_ROOT=C:\Libraries\boost_1_59_0
 dir c:\libraries\boost
 dir c:\libraries\boost_1_59_0
-set BOOST_LIBRARYDIR=C:\Libraries\boost_1_59_0\lib64-msvc-13.0
+set BOOST_LIBRARYDIR=C:\Libraries\boost_1_59_0\lib64-msvc-12.0
 
 echo Running cmake...
 cd c:\projects\tempus
