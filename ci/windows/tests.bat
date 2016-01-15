35beb246922fe4f9728ab70701cbc89e94abc49e
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-15 14:26:02 +0100

    tests7

--- ci/windows/tests.bat
+++ ci/windows/tests.bat
@@ -1,15 +1,21 @@
 cd c:\projects\tempus
-echo Populating test db ...
 SET PGUSER=postgres
 SET PGPASSWORD=Password12!
 SET PATH=C:\Program Files\PostgreSQL\9.3\bin;%PATH%
 SET PGHOST=localhost
+
+echo create db
 createdb tempus_test_db
+echo install postgis extension
 psql -c "create extension postgis;" tempus_test_db
-cd data\tempus\tempus_test_db
+echo unzip data
+cd data\tempus_test_db
 7z x tempus_test_db.sql.zip -o tempus_test_db.sql -y
+echo populate test db
 psql tempus_test_db < tempus_test_db.sql
+echo apply patches
 for /r %%i in (patch.???.sql) do psql tempus_test_db < %%i
 cd c:\projects\tempus
 
+echo run tests ...
 ctest -VV
