3bee9b8588fe8ed740d14b0a0d40c72f9d9845f2
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-15 13:55:53 +0100

    psql2

--- ci/windows/build_appveyor.bat
+++ ci/windows/build_appveyor.bat
@@ -16,7 +16,10 @@ echo Populating test db ...
 SET PGUSER=postgres
 SET PGPASSWORD=Password12!
 SET PATH=C:\Program Files\PostgreSQL\9.3\bin;%PATH%
-psql -c "create database tempus_test_db;"
+SET PGHOST=localhost
+echo create db
+createdb tempus_test_db
+echo postgis
 psql -c "create extension postgis;" tempus_test_db
 7z x data\tempus\tempus_test_db\tempus_test_db.sql.zip -o tempus_test_db.sql -y
 psql tempus_test_db < tempus_test_db.sql
