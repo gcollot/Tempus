e679f1cf2d592a63ffdc38d34ce01137b675fb5e
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-15 14:02:16 +0100

    tests

--- ci/windows/build_appveyor.bat
+++ ci/windows/build_appveyor.bat
@@ -12,21 +12,6 @@ echo Running cmake...
 cd c:\projects\tempus
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_DOC=OFF -DBUILD_OSM2SHP=OFF -DBUILD_QGIS_PLUGIN=OFF -DBUILD_WPS=OFF
 
-echo Populating test db ...
-SET PGUSER=postgres
-SET PGPASSWORD=Password12!
-SET PATH=C:\Program Files\PostgreSQL\9.3\bin;%PATH%
-SET PGHOST=localhost
-echo create db
-createdb tempus_test_db
-echo postgis
-psql -c "create extension postgis;" tempus_test_db
-7z x data\tempus\tempus_test_db\tempus_test_db.sql.zip -o tempus_test_db.sql -y
-psql tempus_test_db < tempus_test_db.sql
-psql tempus_test_db < data\tempus\tempus_test_db\patch.001.sql
-psql tempus_test_db < data\tempus\tempus_test_db\patch.002.sql
-psql tempus_test_db < data\tempus\tempus_test_db\patch.003.sql
-
 nmake
 
 GOTO DONE
